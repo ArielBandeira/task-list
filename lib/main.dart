ad4013a186ed5d1617b69934c4702bfc25e70a22
arielbandeira <arieltwain@gmail.com>
2021-03-30 19:11:25 -0300

    commit main

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,22 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              
+            ),
+          )
+        ],
+      ),
     );
   }
 }
